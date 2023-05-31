d78d4bd1c70cfec943fc9e74d351c7e84bb53384
史蒂夫ZMT工作室 <98326195+SteveZMTstudios@users.noreply.github.com>
2023-05-31 19:10:08 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<windows.h>
+#include<cstdio>
+using namespace std;
+const char MIF_LNK=//lanzou
+const char MIF_DNL=//
+string l[102];
+bool issu=False;
+bool developer_option,enable_log,debug_usr;
